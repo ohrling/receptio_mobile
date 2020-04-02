f8e8586896dc869e65ede9508cfd2c3527b8105e
Tommy Ohrling <ohrling@users.noreply.github.com>
2020-04-02 15:24:19 +0200

    Started to implement the ui

---
+++ lib/features/getrecipe/presentation/pages/recipe_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class RecipePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Hej kocken!'),
+      ),
+    );
+  }
+}
